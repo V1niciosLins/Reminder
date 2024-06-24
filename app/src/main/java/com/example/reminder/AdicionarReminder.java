1a9a001f0c0b5270172ef097f4707ab7b3c25185
Vinicios <viniciuslins61@gmail.com>
2024-06-24 19:17:25 -0300

    Iniciais

--- app/src/main/java/com/example/reminder/AdicionarReminder.java
+++ app/src/main/java/com/example/reminder/AdicionarReminder.java
@@ -8,17 +8,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class AdicionarReminder extends AppCompatActivity {
+import com.example.reminder.databinding.ActivityAdicionarReminderBinding;
 
+public class AdicionarReminder extends AppCompatActivity {
+ActivityAdicionarReminderBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_adicionar_reminder);
+        binding = ActivityAdicionarReminderBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
